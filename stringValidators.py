3a17b18f2b1a56dacbc0322c1ee92236da4ffc2e
“Anis” <anisbelhacel@gmail.com>
2024-11-26 23:55:43 +0100

    Solution for String Validators

---
+++ stringValidators.py
@@ -0,0 +1,18 @@
+
+
+if __name__ == '__main__':
+    s = input("")
+    """
+    # Longer way
+    s_alnum = "False"
+    for char in s:
+        if char.isalnum():
+            s_alnum = "True"
+            break
+    print(s_alnum)
+    """
+    print("True" if any(char.isalnum() for char in s) else "False")
+    print("True" if any(char.isalpha() for char in s) else "False")
+    print("True" if any(char.isdigit() for char in s) else "False")
+    print("True" if any(char.islower() for char in s) else "False")
+    print("True" if any(char.isupper() for char in s) else "False")
